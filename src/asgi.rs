7833615622176d1f331ad5a2cf8011ff98b8d852
ChillFish8 <hburt2003@gmail.com>
2020-10-19 12:03:39 +0100

    add return self callback

--- src/asgi.rs
+++ src/asgi.rs
@@ -12,11 +12,11 @@ use pyo3::types::PyDict;
 
 
 const HIGH_WATER_LIMIT: usize = 64 * 1024;  // 64KiB
-static FRAMEWORK_CALLBACK: OnceCell<PyObject> = OnceCell::new();
+static CALLBACK: OnceCell<PyObject> = OnceCell::new();
 
 
 pub fn setup(callback: PyObject) {
-    let _: &PyObject = FRAMEWORK_CALLBACK.get_or_init(|| {
+    let _: &PyObject = CALLBACK.get_or_init(|| {
         callback
     });
 }
@@ -70,67 +70,16 @@ impl RequestResponseCycle {
 #[pyproto]
 impl PyAsyncProtocol for RequestResponseCycle {
     fn __await__(slf: PyRef<Self>) -> PyResult<()> {
-        let callback: &PyObject = FRAMEWORK_CALLBACK
+        let callback: &PyObject = CALLBACK
             .get()
             .unwrap();
 
 
         let gil = Python::acquire_gil();
         let py = gil.python();
-        let fut = callback.call0(py)?;
+        let fut = callback.call1(py, (slf,))?;
 
 
         Ok(())
-    }
-}
-
-/// ASGISend is the the 'send' coroutine of standard ASGI
-/// servers, this is implemented as a class because it has to be
-/// a coroutine object.
-#[pyclass]
-struct ASGISend {
-    transport: Arc<PyObject>,
-    fc: Arc<protocol::FlowControl>,
-}
-
-impl ASGISend {
-    fn new(transport: Arc<PyObject>, fc: Arc<protocol::FlowControl>) -> Self {
-        ASGISend{
-            transport,
-            fc,
-        }
-    }
-}
-
-#[pymethods]
-impl ASGISend {
-    #[call]
-    fn __call__(&mut self, data: PyDict) {
-        data.get_item("");
-        self
-    }
-}
-
-
-#[pyclass]
-struct ASGIReceive {
-    transport: Arc<PyObject>,
-    fc: Arc<protocol::FlowControl>,
-}
-
-impl ASGIReceive {
-    fn new(transport: Arc<PyObject>, fc: Arc<protocol::FlowControl>) -> Self {
-        ASGIReceive{
-            transport,
-            fc,
-        }
-    }
-}
-
-#[pymethods]
-impl ASGIReceive {
-    #[call]
-    fn __call__(&mut self, data: PyDict) {
-        data.get_item("");
     }
 }
