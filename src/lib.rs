2f630c66467e4f24458287421d72b150f320a967
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-01-18 20:27:29 +0000

    update lib.rs for new build

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,47 @@
-// Pyo3 experimental
+/// Pyre is a HTTP written in Rust for Python, taking inspiration from the
+/// ASGI interface while also building on past servers mistakes and issues.
+///
+/// Aims:
+///     - Support HTTP/1 Protocol
+///     - Support HTTP/2 Protocol
+///     - Support WebSocket Protocol
+///
+
+
+mod pyre_server;
+
+use crate::pyre_server::net::server::Server;
+use crate::pyre_server::net::listener::NoneBlockingListener;
+
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-use mimalloc::MiMalloc;
 
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
-
-mod server;
-
-use server::net::listener;
-use server::net::handler;
-
-
+/// Creates a client handler instance linked to a TcpListener and event loop.
+///
+/// Args:
+///     host:
+///         The given host string to bind to e.g. '127.0.0.1'.
+///     port:
+///         The given port to bind to e.g. 6060.
+///     backlog:
+///         The max amount of iterations to do when accepting clients
+///         when the socket is ready and has been invoked.
+///
+/// Returns:
+///     A un-initialised HandleClients instance linked to the main listener.
 #[pyfunction]
-fn setup(
-    loop_create_task: PyObject,
-    loop_remove_reader: PyObject,
-    loop_remove_writer: PyObject,
-) {
-    handler::setup(
-        loop_create_task,
-        loop_remove_reader,
-        loop_remove_writer,
-    );
+fn create_server(
+    host: &str,
+    port: u16,
+    backlog: usize,
+) -> PyResult<Server> {
+    let binder = format!("{}:{}", host, port);
+
+    let listener = NoneBlockingListener::bind(&binder)?;
+    let new_handler = Server::new(backlog, listener);
+
+    Ok(new_handler)
 }
 
 
@@ -32,8 +50,7 @@ fn setup(
 ///
 #[pymodule]
 fn pyre(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<listener::PyreListener>()?;
-    m.add_class::<handler::PyreClientHandler>()?;
-    m.add_function(wrap_pyfunction!(setup, m)?).unwrap();
+    m.add_function(wrap_pyfunction!(create_server, m)?)?;
+    m.add_class::<Server>()?;
     Ok(())
 }
