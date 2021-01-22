1f7c7477547f78024d18b6c0684f8999f74fada8
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-01-22 19:51:59 +0000

    add python callbacks

--- src/pyre_server/responders/data_callback.rs
+++ src/pyre_server/responders/data_callback.rs
@@ -1,18 +1,22 @@
 use pyo3::prelude::*;
+use pyo3::exceptions::PyRuntimeError;
 
 use crossbeam::channel::Sender;
 use bytes::Bytes;
-use pyo3::exceptions::PyRuntimeError;
 
 
+/// The payload that gets sent to the receiver half of the channel.
 pub type SenderPayload = (bool, Vec<u8>);
 
 
+/// The callable class that handling communication back to the server protocol.
 #[pyclass]
 pub struct DataSender {
     tx: Sender<SenderPayload>,
 }
+
 impl DataSender {
+    /// Create a new handler with the given sender.
     pub fn new(tx: Sender<SenderPayload>) -> Self {
         Self { tx }
     }
@@ -20,6 +24,8 @@ impl DataSender {
 
 #[pymethods]
 impl DataSender {
+    /// Invoked by python passing more_body which represents if there
+    /// is any more body to expect or not, and the body itself.
     #[call]
     fn __call__(&self, more_body: bool, body: Vec<u8>) -> PyResult<()> {
         if let Err(e) = self.tx.send((more_body, body)) {
